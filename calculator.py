3b06967c1c6d6062a955f00e1d2bba02d123f14f
Evan Sonin <43421690+WackyWolff@users.noreply.github.com>
2020-01-09 13:14:09 -0700

    Create calculator.py
    It doesn't work yet

---
+++ calculator.py
@@ -0,0 +1,17 @@
+numberOne = input("What is the first number?\n")
+numberTwo = input("What is the second number?\n")
+operator = input("What is the operator? (Options: * / + -)\n")
+if operator is ("*" or "/" or "+" or "-"){
+        if operator is ("*")
+            result = (numberOne*numberTwo)
+        elif operator is ("/")
+            result = (numberOne/numberTwo)
+        elif operator is ("+")
+            result = (numberOne+numberTwo)
+        elif operator is ("-")
+            result = numberOne-numberTwo
+    }
+    elif operator is not ("*" or "/" or "+" or "-")
+        print("That is not a valid operator!")
+result = (numberOne+numberTwo)
+print(result)
