f11717666d1c7f563c7c7416b9ebcc7902ebe2c1
Evan Sonin <43421690+WackyWolff@users.noreply.github.com>
2020-01-10 10:39:37 -0700

    It actually works now
    Completely redid the program.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Really Bad Python Calculator
  A really bad calculator, that can only accept two variables at a time (at least, at the time being), because I have no idea what I'm doing.
+
--- calculator.py
+++ calculator.py
@@ -1,17 +1,28 @@
-numberOne = input("What is the first number?\n")
-numberTwo = input("What is the second number?\n")
-operator = input("What is the operator? (Options: * / + -)\n")
-if operator is ("*" or "/" or "+" or "-"){
-        if operator is ("*")
-            result = (numberOne*numberTwo)
-        elif operator is ("/")
-            result = (numberOne/numberTwo)
-        elif operator is ("+")
-            result = (numberOne+numberTwo)
-        elif operator is ("-")
-            result = numberOne-numberTwo
-    }
-    elif operator is not ("*" or "/" or "+" or "-")
-        print("That is not a valid operator!")
-result = (numberOne+numberTwo)
+numberOne = float(input("What is the first number?\n"))
+numberTwo = float(input("What is the second number?\n"))
+operator = (input("What is the operator? (Options: * / + -)\n"))
+
+def subtract(numOne, numTwo):
+    return numOne-numTwo
+def add(numOne, numTwo):
+    return numOne+numTwo
+def multiply(numOne, numTwo):
+    return numOne*numTwo
+def divide(numOne, numTwo):
+    return numOne/numTwo
+if operator is ("*"):
+    result = (multiply(numberOne, numberTwo))
+if operator is ("/"):
+    if (numberTwo==0):
+        result = ("Error. Cannot divide by 0. Please run the program again, and try not to be stupid.")
+    else:
+        result = (divide(numberOne, numberTwo))
+if operator is ("+"):
+    result = (add(numberOne, numberTwo))
+if operator is ("-"):
+    result = (subtract(numberOne,numberTwo))
+if operator is not ("+" or "-" or "*" or "/"):
+    result=("That isn't an operator. Please run the program again, and try not to be stupid.")
+print("\n")
 print(result)
+print("\n")
